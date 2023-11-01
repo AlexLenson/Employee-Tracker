4b08f6050605f31f27baae2a2ad093f61b9e1c16
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-11-01 15:43:10 -0400

    updated options

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const { prompt } = require("inquirer");
 
 const options = [
-    "view employees",
-    "add employees",
-    "update an employee"
+    "view all departments",
+    "view all roles",
+    "view all employees",
+    "add a department",
+    "add a role",
+    "add an employee",
+    "update an employee role"
 ];
+
+const question = {
+    type: "list",
+    name: "selectedOption",
+    message: "What do you want to do?",
+    choices: options
+};
+
